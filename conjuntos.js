2f71989e284705def03f19bd44029cdb2811bc9b
Axel Rodriguez <axelrprado@gmail.com>
2023-04-10 17:47:45 -0600

    Ejercicio 7

---
+++ conjuntos.js
@@ -0,0 +1,10 @@
+// Crea un archivo llamado conjuntos.js que contenga las siguientes líneas
+// - Un nuevo Set con los nombres de tu familia
+const array= ["Axel", "Anneth", "Amelia"]
+const miSet2 = new Set(array)
+
+// - Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
+miSet2.add("Axel")
+
+// - Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;)
+miSet2.add("Javascript")
